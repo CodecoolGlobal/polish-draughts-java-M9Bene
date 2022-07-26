11cb4e3abdaf59f2c70d7584e70072df49e16e03
Andras Csovari <csovaria@gmail.com>
2022-07-26 13:33:04 +0200

    implement create pawns test version

--- src/main/java/com/codecool/polish/Board.java
+++ src/main/java/com/codecool/polish/Board.java
@@ -13,11 +13,33 @@ public class Board {
     }
 
     public void createPawns() {
-       fields[0][0] = new Pawn("Black");
+        int[] emptyLines = new int[2];
+        if (size % 2 == 1){
+            emptyLines[0] = size/2+1;
+            emptyLines[1] = 100000;
+        } else {
+            emptyLines[0] = size/2+1;
+            emptyLines[1] = size/2;
+        }
+        System.out.println(emptyLines[0]);
+        int fieldCounter = 1;
+            for(int x = 0; x < this.size; x ++){
+                for (int y = 0; y < this.size; y ++){
+                    if (fieldCounter % 2 == 1){
+                        fieldCounter ++;
+//                        if (y != emptyLines[0] && y != emptyLines[1]){
+                            fields[y][x] = new Pawn("black");
+//                        }
+                    } else {
+                        fieldCounter ++;
+                    }
+                }
+            }
+//       fields[0][0] = new Pawn("Black");
        System.out.println(Arrays.deepToString(fields));
     }
 
     public void printBoard() {
-        System.out.println(fields[0][0].getColor());
+//        System.out.println(fields[0][0].getColor());
     }
 }
